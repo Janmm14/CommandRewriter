d6c17f2f233e9464039d14adf795ed269ead02ea
Janmm14 <computerjanimaus@yahoo.de>
2015-10-30 15:43:09 +0100

    Add @NotNull annotations and some javadoc to CommandRewriteEvent

--- src/main/java/me/minotopia/commandrewriter/CommandRewriteEvent.java
+++ src/main/java/me/minotopia/commandrewriter/CommandRewriteEvent.java
@@ -4,21 +4,25 @@ import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class CommandRewriteEvent extends Event implements Cancellable {
 
+    @NotNull
     private final PlayerCommandPreprocessEvent commandPreprocessEvent;
+    @NotNull
     private final String fullIssuedCommand;
+    @NotNull
     private final String rewriteTrigger;
+    @NotNull
     private List<String> messageToSend;
     private boolean cancelled;
+    @NotNull
     private CommandRewriteEvent.Unsafe unsafe = new CommandRewriteEvent.Unsafe();
 
-    public CommandRewriteEvent(PlayerCommandPreprocessEvent commandPreprocessEvent, String fullIssuedCommand, String rewriteTrigger, List<String> messageToSend) {
+    public CommandRewriteEvent(@NotNull PlayerCommandPreprocessEvent commandPreprocessEvent, @NotNull String fullIssuedCommand, @NotNull String rewriteTrigger, @NotNull List<String> messageToSend) {
         this.commandPreprocessEvent = commandPreprocessEvent;
         this.fullIssuedCommand = fullIssuedCommand;
         this.rewriteTrigger = rewriteTrigger;
@@ -28,43 +32,33 @@ public class CommandRewriteEvent extends Event implements Cancellable {
     /**
      * optain unsafe methods of this event
      */
+    @NotNull
     public CommandRewriteEvent.Unsafe unsafe() {
         return unsafe;
     }
 
-    public class Unsafe {
-        public PlayerCommandPreprocessEvent getCommandPreprocessEvent() {
-            return commandPreprocessEvent;
-        }
-    }
-
+    /**
+     * @return The full command issued
+     */
+    @NotNull
     public String getFullIssuedCommand() {
         return fullIssuedCommand;
     }
 
+    /**
+     * @return The part of the issued command which is defined in the command rewriter config and lead to rewrite the command
+     */
+    @NotNull
     public String getRewriteTrigger() {
         return rewriteTrigger;
     }
 
     /**
-     * @return A (mutable) copy of the message which will be sent.
+     * @return The message which will be sent, mutable, no copy
      */
+    @NotNull
     public List<String> getMessageToSend() {
-        return new ArrayList<>(messageToSend);
-    }
-
-    /**
-     * @param messageToSend The message which should be send. List entries are the lines. Set to {@code null} if you want to suppress the message.
-     */
-    public void setMessageToSend(List<String> messageToSend) {
-        this.messageToSend = messageToSend;
-    }
-
-    /**
-     * @see #setMessageToSend(List)
-     */
-    public void setMessageToSend(String... messageToSend) {
-        this.messageToSend = Arrays.asList(messageToSend);
+        return messageToSend;
     }
 
     /**
@@ -77,23 +71,31 @@ public class CommandRewriteEvent extends Event implements Cancellable {
     }
 
     /**
-     * Whether to cancel the command rewriting.
+     * Whether to cancel the command rewriting.<br>
+     * <br>
      * Not cancelled will do this:
      * <ul>
-     * <li>Cancel the {@link org.bukkit.event.player.PlayerCommandPreprocessEvent}</li>
+     * <li>Cancel the {@link PlayerCommandPreprocessEvent}</li>
      * <li>Sending the message optainable via {@link #getMessageToSend()} to the player</li>
      * </ul>
      * Cancelled will do this:
      * <ul>
-     * <li>Not changing the state of the {@link org.bukkit.event.player.PlayerCommandPreprocessEvent}</li>
+     * <li>Not changing the state of the {@link PlayerCommandPreprocessEvent}</li>
      * <li>Not sending any message to the player.</li>
      * </ul>
      *
-     * @param cancelled
+     * @param cancelled whehter to cancel command rewriting
      */
     @Override
     public void setCancelled(boolean cancelled) {
         this.cancelled = cancelled;
+    }
+
+    public class Unsafe {
+        @NotNull
+        public PlayerCommandPreprocessEvent getCommandPreprocessEvent() {
+            return commandPreprocessEvent;
+        }
     }
 
     //////////////////////// Needed for custom events ////////////////////////
